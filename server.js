a48446fa4b6cc7f1e09fb441565c627674210674
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-10-15 18:28:39 -0700

    Use S3 for caching

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ server.use(prerender.blacklist());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
-server.use(require("prerender-memcached-cache"));
+
+server.use(prerender.s3HtmlCache());
+//server.use(require("prerender-memcached-cache"));
 
 server.start();
