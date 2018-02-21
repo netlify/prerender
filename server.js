03c7425b789148069e8062beacc42d50b854125f
Bryce Kahle <bkahle@gmail.com>
2018-02-21 17:58:23 -0500

    Use chrome version of prerender and make changes via plugins

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-#!/usr/bin/env node
-var prerender = require('./lib');
+"use strict";
 
-var server = prerender({
-    workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
-});
+const prerender = require("prerender");
+const server = prerender();
 
-server.use({
-  beforePhantomRequest: function(req,res,next) {
-    if (req.prerender.url == process.env.STATUS_URL) {
-       var username = process.env.BASIC_AUTH_USERNAME;
-       var password = process.env.BASIC_AUTH_PASSWORD;
-       var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-       req.headers.authorization = auth;
-    }
-    next();
-  }
-});
-
+server.use(require("./lib/plugins/healthCheckAuth"));
 server.use(prerender.basicAuth());
 server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
-
-server.use(prerender.s3HtmlCache());
-//server.use(require("prerender-memcached-cache"));
+server.use(require("./lib/plugins/s3HtmlCache"));
 
 server.start();
